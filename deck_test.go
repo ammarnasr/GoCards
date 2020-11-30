2f462b92cdb2b4f3bee7aef749077e0139a31f80
ammarnasr <ammarnasraza@gmail.com>
2020-11-30 02:40:24 +0200

    more cases

--- deck_test.go
+++ deck_test.go
@@ -6,7 +6,15 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Expected Length of 16 but got: %d", len(d))
+		t.Errorf("Expected Length of 16 but got: %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of  but got: %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Diamonds" {
+		t.Errorf("Expected last card of  but got: %v", d[len(d)-1])
 	}
 
 }
